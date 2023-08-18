505e78bbab8b621a8ebe54f8005b71a77eacd1e1
i-am-harveyt <haotingtong30@gmail.com>
2023-08-18 09:00:02 +0800

    Add: fetchProfile API; Fix: Error in Doc

---
+++ api/1.0/controller/users/fetchProfile.controller.js
@@ -0,0 +1,29 @@
+import fetchProfile from "../../model/users/fetchProfile.model.js";
+import {
+  ECONNREFUSED,
+  ER_WRONG_VALUE_FOR_TYPE,
+} from "../../util/sqlErr.util.js";
+
+/**
+ * @param {import('express').Request} req
+ * @param {import('express').Response} res
+ * @param {import('express').NextFunction} next
+ */
+export default async function fetchProfileHandler(req, res, next) {
+  const userId = req.user_id;
+  const targetId = req.params.user_id;
+  let result = null;
+  try {
+    [result] = await fetchProfile(userId, targetId);
+  } catch (err) {
+    switch (err.errno) {
+      case ECONNREFUSED.errno:
+        return res.status(500).json({ error: ECONNREFUSED.message });
+      case ER_WRONG_VALUE_FOR_TYPE.errno:
+        return res.status(403).json({ error: ER_WRONG_VALUE_FOR_TYPE.message });
+      default:
+        return res.status(400).json({ error: err });
+    }
+  }
+  return res.status(200).json({ data: { user: result } });
+}
