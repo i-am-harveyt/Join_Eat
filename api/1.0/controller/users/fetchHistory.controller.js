6f90044f230a0094847938831ac9fd36b3639d56
i-am-harveyt <haotingtong30@gmail.com>
2023-08-18 10:10:16 +0800

    Fix: time format

--- api/1.0/controller/users/fetchHistory.controller.js
+++ api/1.0/controller/users/fetchHistory.controller.js
@@ -1,7 +1,8 @@
+import transformTimeFormat from "../../util/transfromTimeFormat.util.js";
 import fetchHistory from "../../model/users/fetchHistory.model.js";
 import {
-  ECONNREFUSED,
-  ER_WRONG_VALUE_FOR_TYPE,
+	ECONNREFUSED,
+	ER_WRONG_VALUE_FOR_TYPE,
 } from "../../util/sqlErr.util.js";
 
 /**
@@ -10,22 +11,30 @@ import {
  * @param {import('express').NextFunction} next
  */
 export default async function fetchHistoryHandler(req, res, next) {
-  const userId = req.user_id;
+	const userId = req.user_id;
 
-  let result = null;
-  try {
-    [result] = await fetchHistory(userId);
-  } catch (err) {
-    switch (err.errno) {
-      case ECONNREFUSED.errno:
-        return res.status(500).json({ error: ECONNREFUSED.message });
-      case ER_WRONG_VALUE_FOR_TYPE:
-        return res.status(403).json({ error: ER_WRONG_VALUE_FOR_TYPE.message });
+	let result = null;
+	try {
+		[result] = await fetchHistory(userId);
+	} catch (err) {
+		switch (err.errno) {
+			case ECONNREFUSED.errno:
+				return res.status(500).json({ error: ECONNREFUSED.message });
+			case ER_WRONG_VALUE_FOR_TYPE:
+				return res.status(403).json({ error: ER_WRONG_VALUE_FOR_TYPE.message });
 
-      default:
-        return res.status(400).json({ error: err });
-    }
-  }
+			default:
+				return res.status(400).json({ error: err });
+		}
+	}
+	const ret = result.map(
+		row => {
+			return {
+				...row,
+				appointment_time: transformTimeFormat(row.appointment_time),
+			}
+		},
+	);
 
-  return res.status(200).json({ data: { events: result } });
+	return res.status(200).json({ data: { events: ret } });
 }
--- api/1.0/model/users/fetchHistory.model.js
+++ api/1.0/model/users/fetchHistory.model.js
@@ -4,7 +4,7 @@ const query = `
 SELECT
 BIN_TO_UUID(id) AS id, name, shop_name,
 is_public, latitude, longitude,
-created_at, appointment_time, people_limit,
+appointment_time, people_limit,
 people_joined, status
 FROM events WHERE host_id=UUID_TO_BIN(?) ORDER BY appointment_time;
 `;
