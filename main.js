2f049815001bcd8697fd919177b9de463c0d8b3f
陳樂燊 <chenleshen@LSs-MacBook-Pro.local>
2023-08-21 17:52:54 +0800

    Add: static route

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ import api from "./api/1.0/api.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/static", express.static("./static"))
 const port = process.env.JOIN_EAT_PORT;
 
 app.get("/", (req, res) => {
