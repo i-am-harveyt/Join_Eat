f5cb51a26161490f8cdd3afecef68d11010c1077
陳樂燊 <chenleshen@LSs-MacBook-Pro.local>
2023-08-23 14:18:46 +0800

    Fix: del picticre

--- api/1.0/model/events/rangeShop.model.js
+++ api/1.0/model/events/rangeShop.model.js
@@ -3,7 +3,6 @@ import db from "../db.js";
 
 const searchQuery = `
 SELECT BIN_TO_UUID(id) AS event_id, BIN_TO_UUID(host_id) AS host_id, 
-( SELECT picture FROM users WHERE users.id = events.host_id ) AS picture,
 name, shop_name, latitude, longitude, people_limit, people_joined, appointment_time, is_public,
 Floor(ST_Distance_Sphere(POINT(?, ?), POINT(?, ?))) AS distance,
 ( SELECT COUNT(*) FROM participants WHERE participants.user_id = UUID_TO_BIN(?) AND participants.event_id = events.id ) AS is_joined
